7fb13db911e1eb90496eaf4b7b8997b91150c124
TQTL <tqtl@tqtl.org>
2018-11-29 10:56:33 +0800

    项目初始化提交！

--- apps/taskorder/admin.py
+++ apps/taskorder/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.site_title = "崔晓昭的Django后台管理"
+admin.site.site_title = "崔晓昭的Django后台管理系统"
 admin.site.site_header = "TQTL'S Django"
